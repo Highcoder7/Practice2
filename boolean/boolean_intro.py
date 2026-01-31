a9c28928f58b411db7fb9fb64a7fffd8f9fd91de
Highcoder7 <you@example.com>
2026-01-31 11:12:10 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ boolean/boolean_intro.py
@@ -0,0 +1,18 @@
+print(bool("Hello"))
+print(bool(15))
+print(bool(0))
+print(bool([]))
+print(bool(None))
+
+
+
+
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+print(bool(0))
+print(bool([]))
+print(bool(None))
